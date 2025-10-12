f222120ff705e1604a3ccfe0de6a263a7f6d6ec3
Credipaz Sistemas <159343108+credipazgithub@users.noreply.github.com>
2025-10-12 10:19:04 -0300

    Add files via upload

--- git_fetch.bat
+++ git_fetch.bat
@@ -1,4 +1,23 @@
+%windir%\System32\inetsrv\appcmd stop apppool /apppool.name:C1-Testing.Servicio.NeoCore
+%windir%\System32\inetsrv\appcmd stop site /site.name:C1-Testing.Servicio.NeoCore
+%windir%\System32\inetsrv\appcmd stop apppool /apppool.name:C2-Testing.Servicio.CPFinancials
+%windir%\System32\inetsrv\appcmd stop site /site.name:C2-Testing.Servicio.CPFinancials
+%windir%\System32\inetsrv\appcmd stop apppool /apppool.name:C2-Testing.Servicio.FacturaElectronica
+%windir%\System32\inetsrv\appcmd stop site /site.name:C2-Testing.Servicio.FacturaElectronica
+%windir%\System32\inetsrv\appcmd stop apppool /apppool.name:C3-Testing.Sistema.Interfaces
+%windir%\System32\inetsrv\appcmd stop site /site.name:C3-Testing.Sistema.Interfaces
+
 git fetch origin
 git reset --hard origin/main
 git clean -fd
+
+%windir%\System32\inetsrv\appcmd start apppool /apppool.name:C1-Testing.Servicio.NeoCore
+%windir%\System32\inetsrv\appcmd start site /site.name:C1-Testing.Servicio.NeoCore
+%windir%\System32\inetsrv\appcmd start apppool /apppool.name:C2-Testing.Servicio.CPFinancials
+%windir%\System32\inetsrv\appcmd start site /site.name:C2-Testing.Servicio.CPFinancials
+%windir%\System32\inetsrv\appcmd start apppool /apppool.name:C2-Testing.Servicio.FacturaElectronica
+%windir%\System32\inetsrv\appcmd start site /site.name:C2-Testing.Servicio.FacturaElectronica
+%windir%\System32\inetsrv\appcmd start apppool /apppool.name:C3-Testing.Sistema.Interfaces
+%windir%\System32\inetsrv\appcmd start site /site.name:C3-Testing.Sistema.Interfaces
+
 pause
